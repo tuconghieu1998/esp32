febad23d43e1d3ad88dcafa2144da748bcce06a0
DESKTOP-P71AP16\ASUS1 <tuconghieu1998@gmail.com>
2025-04-25 14:30:24 +0700

    read data from socket server

--- routes/machine/index.route.js
+++ routes/machine/index.route.js
@@ -4,9 +4,11 @@ import expressWs from 'express-ws';
 import { authenticate } from '../../middlewares/middleware.js';
 import { WebSocketServer } from 'ws';
 expressWs(router); // Enable WebSocket support on this router
+import axios from 'axios';
 
-const DELAY_SEND_WS = 5000; 
+const DELAY_SEND_WS = 5000;
 const clients = new Set();
+const MACHINE_200_SERVER_URL = process.env.MACHINE_200_SERVER_URL;
 
 router.get('/', authenticate, (req, res, next) => {
     res.render('machine');
@@ -29,15 +31,42 @@ router.ws('/socket/ws2', (ws, req) => {
     });
 });
 
-// Function to simulate and broadcast machine data
-function broadcastMachineData() {
-    const machines = generateMachines(200);
+// Function to fetch machine data
+async function getMachineData() {
+    try {
+        const response = await axios.get(MACHINE_200_SERVER_URL);
+        if (response.status == 200) {
+            const machineData = response.data;
+            return machineData;
+            // Do something with the machine data, such as broadcasting it to clients
+        }
+        return [];
+    } catch (error) {
+        console.error('Error fetching machine data:', error);
+        return [];
+    }
+}
 
-    const data = JSON.stringify(machines);
-
+function hasClientReady() {
     for (const client of clients) {
         if (client.readyState === 1) {
-            client.send(data);
+            return true;
+        }
+    }
+    return false;
+}
+
+// Function to simulate and broadcast machine data
+async function broadcastMachineData() {
+    if (hasClientReady()) {
+        const machines = await getMachineData();
+        if(!machines || machines.length == 0) return;
+        const data = JSON.stringify(Object.values(machines));
+
+        for (const client of clients) {
+            if (client.readyState === 1) {
+                client.send(data);
+            }
         }
     }
 }
